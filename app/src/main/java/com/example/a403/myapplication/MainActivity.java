1ffe49c201438f37152129dfdcd69b141dc496d3
jeong da seul <ds1417@naver.com>
2016-12-02 14:36:40 +0900

    1.레이아웃 생성

--- app/src/main/java/com/example/a403/myapplication/MainActivity.java
+++ app/src/main/java/com/example/a403/myapplication/MainActivity.java
@@ -12,80 +12,47 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Switch;
 import android.widget.Toast;
-
+import android.widget.Chronometer;
+import android.widget.TextView;
+import android.widget.CalendarView;
+import android.widget.TimePicker;
 import static com.example.a403.myapplication.R.id.radioButton;
 
 public class MainActivity extends AppCompatActivity {
-    Switch S1;
-    LinearLayout L1;
-    RadioGroup R1;
-    RadioButton RB1, RB2, RB3;
-    ImageView I1;
+    LinearLayout LL;
+    RadioGroup RG;
+    RadioButton RB1, RB2;
     Button B1, B2;
+    Chronometer CM;
+    TextView TV;
+    CalendarView CV;
+    TimePicker TP;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        S1 = (Switch) findViewById(R.id.switch1);
-        L1 = (LinearLayout) findViewById(R.id.activity_main);
-        R1 = (RadioGroup) findViewById(R.id.RG);
+        B1 = (Button) findViewById(R.id.button);      // 예약시작 버튼
+        B2 = (Button) findViewById(R.id.button2);
         RB1 = (RadioButton) findViewById(R.id.radioButton);
         RB2 = (RadioButton) findViewById(R.id.radioButton2);
-        RB3 = (RadioButton) findViewById(R.id.radioButton3);
-        I1 = (ImageView) findViewById(R.id.imageView);
-        B1 = (Button) findViewById(R.id.button3);
-        B2 = (Button) findViewById(R.id.button4);
-
-        S1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked == true)
-                    L1.setVisibility(View.VISIBLE);
-                else
-                    L1.setVisibility(View.INVISIBLE);
-            }
-        });
-        RB1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked)
-                    I1.setImageResource(R.drawable.jel);
-            }
-        });
-        RB2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked)
-                    I1.setImageResource(R.drawable.kit);
-            }
-        });
-        RB3.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked)
-                    I1.setImageResource(R.drawable.rol);
-            }
-        });
+        CV = (CalendarView) findViewById(R.id.calendarView);
+        TP = (TimePicker) findViewById(R.id.timePicker);
+        TV = (TextView) findViewById(R.id.textView);
+        CM = (Chronometer) findViewById(R.id.chronometer);
 
         B1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "종료합니다.", Toast.LENGTH_SHORT).show();
 
-                finish();
-            }
+        }
         });
 
-        B2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                S1.setChecked(false);
-                L1.setVisibility(View.INVISIBLE);
-                Toast.makeText(getApplicationContext(),"처음으로 돌아갑니다.",Toast.LENGTH_SHORT).show();
-            }
-        });
+
+
+
+
+
     }
 }
