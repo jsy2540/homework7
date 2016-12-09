49aa49b348a8ba4309767095822bf90867d50b5c
201344033 <jsy2540@naver.com>
2016-12-09 14:26:17 +0900

    1.예약시작 on/off

--- app/src/main/java/com/example/a403/myapplication/MainActivity.java
+++ app/src/main/java/com/example/a403/myapplication/MainActivity.java
@@ -19,11 +19,9 @@ import android.widget.Chronometer;
 import android.widget.TextView;
 import android.widget.CalendarView;
 import android.widget.TimePicker;
-import static com.example.a403.myapplication.R.id.radioButton;
 
 public class MainActivity extends AppCompatActivity {
-    LinearLayout LL;
-    RadioGroup RG;
+
     RadioButton RB1, RB2;
     Button B1, B2;
     Chronometer CM;
@@ -31,60 +29,17 @@ public class MainActivity extends AppCompatActivity {
     CalendarView CV;
     TimePicker TP;
     String date="";
+    Switch SW;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        B1 = (Button) findViewById(R.id.button);
-        B2 = (Button) findViewById(R.id.button2);
-        RB1 = (RadioButton) findViewById(R.id.radioButton);
-        RB2 = (RadioButton) findViewById(R.id.radioButton2);
-        CV = (CalendarView) findViewById(R.id.calendarView);
-        TP = (TimePicker) findViewById(R.id.timePicker);
-        TV = (TextView) findViewById(R.id.textView);
+
         CM = (Chronometer) findViewById(R.id.chronometer);
+        SW = (Switch) findViewById(R.id.reserve_switch);
 
-        B1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                CM.setBase(SystemClock.elapsedRealtime());
-                CM.start();
-                CM.setTextColor(Color.RED);
-            }
-        });
-        RB1.setChecked(true);
-        RB1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                CV.setVisibility(View.VISIBLE);
-                TP.setVisibility(View.INVISIBLE);
-            }
-        });
-        RB2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                CV.setVisibility(View.INVISIBLE);
-                TP.setVisibility(View.VISIBLE);
-            }
-        });
 
-        CalendarView calendarView = (CalendarView)findViewById(R.id.calendarView);
-        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
-            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
-                date = year + "년"+ month + "월" + dayOfMonth + "일";
-            }
-        });
-
-        B2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                CM.stop();
-                CM.setTextColor(Color.BLUE);
-                String time =  TP.getCurrentHour() + "시" + TP.getCurrentMinute() + "분";
-                TV.setText( date + time +"예약됨");
-            }
-        });
     }
 }
